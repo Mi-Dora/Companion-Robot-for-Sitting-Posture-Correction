9891184bf440ef051e78070c7280c93a8b5383fb
midora <mi-dora@qq.com>
2023-04-15 23:57:19 -0400

    fix bug

--- src/image_inspector.py
+++ src/image_inspector.py
@@ -1,12 +1,26 @@
 import cv2
 import os
 
-path = '../dataset/side'
+path = '../dataset/side/control'
+try:
+    with open("{}.log".format(path), 'r') as f:
+        lines = f.readlines()
+        normal = int(lines[1].split(':')[1])
+        bad = int(lines[2].split(':')[1])
+        total = int(lines[3].split(':')[1])
+except FileNotFoundError:
+    normal = 0
+    bad = 0
+    total = 0
 
-normal = 0
-bad = 0
 for root, _, files in os.walk(path):
-    for file in files:
+    files.sort()
+    for i, file in enumerate(files):
+        if file.startswith('.'):
+            continue
+        if i < total:
+            continue
+
         img = cv2.imread(os.path.join(root, file))
         cv2.imshow("Inspector", img)
         while True:
@@ -19,5 +33,10 @@ for root, _, files in os.walk(path):
                 bad += 1
                 print("{}, bad = {}".format(file, bad))
                 break
+        with open("{}.log".format(path), 'w') as f:
+            f.writelines(file + "\n")
+            f.writelines("normal:{}\n".format(normal))
+            f.writelines("bad:{}\n".format(bad))
+            f.writelines("total:{}\n".format(normal+bad))
         cv2.destroyWindow("Inspector")
 print("Healthy Posture Ratio = ", normal / (normal+bad))
--- src/utils.py
+++ src/utils.py
@@ -7,9 +7,12 @@ import math
 
 
 def video2image(video_file, image_path, interval=120):
-    # os.makedirs(os.path.join(image_path, os.path.basename(video_file).split('.')[0]), exist_ok=True)
+    print("Begin: {}".format(video_file))
+    os.makedirs(image_path, exist_ok=True)
     # Open the input movie file
     input_movie = cv2.VideoCapture(video_file)
+    fps = int(round(input_movie.get(cv2.CAP_PROP_FPS)))
+    interval = interval * fps / 30
     cnt = 0
     while True:
         ret, frame = input_movie.read()
@@ -26,7 +29,16 @@ def video2image(video_file, image_path, interval=120):
 
 
 if __name__ == '__main__':
-    path = "/Users/midora/Library/CloudStorage/OneDrive-GeorgiaInstituteofTechnology/MyDocs/CS 7633/Project/UserStudy"
-    for root, _, files in os.walk(path):
+    control = "/Users/midora/Library/CloudStorage/OneDrive-GeorgiaInstituteofTechnology/MyDocs/CS 7633/Project/UserStudy/control"
+    app = "/Users/midora/Library/CloudStorage/OneDrive-GeorgiaInstituteofTechnology/MyDocs/CS 7633/Project/UserStudy/app"
+    robot = "/Users/midora/Library/CloudStorage/OneDrive-GeorgiaInstituteofTechnology/MyDocs/CS 7633/Project/UserStudy/robot"
+    for root, _, files in os.walk(control):
         for file in files:
-            video2image(os.path.join(root, file), '../dataset/side')
+            video2image(os.path.join(root, file), '../dataset/side/control')
+    for root, _, files in os.walk(app):
+        for file in files:
+            video2image(os.path.join(root, file), '../dataset/side/app')
+    for root, _, files in os.walk(robot):
+        for file in files:
+            video2image(os.path.join(root, file), '../dataset/side/robot')
+    print("Done!")
